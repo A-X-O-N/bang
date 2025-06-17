3136ce0912d0f697d2e41907a8dfcc3c1736efff
A-X-O-N <60752716+A-X-O-N@users.noreply.github.com>
2025-06-17 18:16:16 +0200

    Delete data/javascript directory

--- data/javascript/script-form-test.js
+++
@@ -1,9 +0,0 @@
-var SubMess = document.getElementById("submit-message");
-
-function TestFunctionShow()
-  {
-    SubMess.classList.remove("submit-message-not-displayed");
-    SubMess.innerHTML = "Message after submitting the form. <span style='color: green; font-weight: bold;'>Success!</span> or <span style='color: red; font-weight: bold;'>Error!</span>";
-    
-    alert("Submit button was clicked");
-  }
